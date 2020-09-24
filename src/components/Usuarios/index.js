e3a0cb54feb9dc1d4782104667c0084eeb50fca7
hadiazb <hadiazb@unal.edu.co>
2020-09-24 00:23:29 -0500

    add redux cicle

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import { connect } from 'react-redux';
 
 import * as usuariosActions from '../../actions/usuariosActions';
 class Usuarios extends Component {
-	// async componentDidMount() {
-	// 	const response = await axios.get(
-	// 		'http://jsonplaceholder.typicode.com/users'
-	// 	);
-	// 	this.setState({
-	// 		usuarios: response.data,
-	// 	});
-	// }
+	componentDidMount() {
+		this.props.traerTodos();
+	}
 
 	ponerFilas = () =>
 		this.props.usuarios.map((usuario) => (
@@ -23,7 +17,6 @@ class Usuarios extends Component {
 		));
 
 	render() {
-		console.log(this.props);
 		return (
 			<div className='container-fluid pt-4 pb-2'>
 				<table className='table table-striped table-dark table-bordered table-hover table-responsive-sm'>
@@ -45,6 +38,7 @@ const mapStateToProps = (reducers) => {
 	return reducers.usuariosReducer;
 };
 
-export default connect(mapStateToProps, {
-	usuariosActions,
-})(Usuarios);
+export default connect(
+	mapStateToProps,
+	usuariosActions
+)(Usuarios);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers/index';
 
 const store = createStore(
 	reducers, // Todos los reduces
-	{} //Estado inicial
+	{}, //Estado inicial
+	applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
